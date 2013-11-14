870818a87aa6c409bd0d6bdd6e80c67ca83f43cb
patrickmj <patrickmjchnm@gmail.com>
2013-11-14 14:24:17 -0500

    switch to dev commons site

--- CommonsPlugin.php
+++ CommonsPlugin.php
@@ -3,12 +3,12 @@
 define('COMMONS_PLUGIN_DIR', PLUGIN_DIR . '/Commons');
 define('COMMONS_BASE_URL', 'http://localhost/commons');
 
-// /*
+ /*
 define('COMMONS_API_URL', 'http://localhost/commons/commons-api/import');
 define('COMMONS_API_SETTINGS_URL', 'http://localhost/commons/commons-api/site/');
 // */
 
- /*
+// /*
 define('COMMONS_API_URL', 'http://dev.omeka.org/omeka-commons/commons-api/import');
 define('COMMONS_API_SETTINGS_URL', 'http://dev.omeka.org/omeka-commons/commons-api/site/');
 // */
